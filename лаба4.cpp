63a4b0596d1016dbb4c2d47791ff4cb9be439da3
BoychikFromAsia <igorganchannel@gmail.com>
2024-12-07 17:03:10 +0300

    Add files via upload
    vers 3

--- лаба4.cpp
+++ лаба4.cpp
@@ -1,23 +1,23 @@
 ﻿#include <iostream>
 #include <iomanip>
-
 using namespace std;
-void inputMatrix(double matrix[][10], int n, int m) {
+
+void inputMatrix(double** matrix, int n, int m) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             cin >> matrix[i][j];
         }
     }
 }
-void printMatrix(double matrix[][10], int n, int m) {
+void printMatrix(double** matrix, int n, int m) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
-            cout << fixed << setprecision(2) << matrix[i][j] << " ";
+            cout << fixed << setprecision(2) << matrix[i][j] << (j < m - 1 ? " " : "");
         }
         cout << endl;
     }
 }
-void multiplyMatrix(double A1[][10], double A2[][10], double result[][10], int N, int M, int K) {
+void multiplyMatrix(double** A1, double** A2, double** result, int N, int M, int K) {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < K; ++j) {
             result[i][j] = 0;
@@ -27,42 +27,61 @@ void multiplyMatrix(double A1[][10], double A2[][10], double result[][10], int N
         }
     }
 }
-void matrixPower(double A[][10], double result[][10], int x, int n) {
+void matrixPower(double** A, double** result, int x, int n) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            result[i][j] = (i == j) ? 1 : 0;
+            result[i][j] = (i == j) ? 1 : 0; 
         }
     }
-    double base[10][10];
+    double** base = new double* [n];
     for (int i = 0; i < n; ++i) {
+        base[i] = new double[n];
         for (int j = 0; j < n; ++j) {
             base[i][j] = A[i][j];
         }
     }
     while (x > 0) {
         if (x % 2 == 1) {
-            double temp[10][10];
+            double** temp = new double* [n];
+            for (int i = 0; i < n; ++i) {
+                temp[i] = new double[n];
+            }
             multiplyMatrix(result, base, temp, n, n, n);
             for (int i = 0; i < n; ++i) {
                 for (int j = 0; j < n; ++j) {
                     result[i][j] = temp[i][j];
                 }
             }
+            for (int i = 0; i < n; ++i) {
+                delete[] temp[i];
+            }
+            delete[] temp;
         }
-        double temp[10][10];
+        double** temp = new double* [n];
+        for (int i = 0; i < n; ++i) {
+            temp[i] = new double[n];
+        }
         multiplyMatrix(base, base, temp, n, n, n);
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
                 base[i][j] = temp[i][j];
             }
         }
+        for (int i = 0; i < n; ++i) {
+            delete[] temp[i];
+        }
+        delete[] temp;
         x /= 2;
     }
+
+    for (int i = 0; i < n; ++i) {
+        delete[] base[i];
+    }
+    delete[] base;
 }
 int main(int argc, const char* argv[]) {
     setlocale(LC_ALL, "RU");
     bool isHuman = (argc <= 1 || strcmp(argv[1], "false") != 0);
-    const int MAX_SIZE = 10;
     int N, M;
     if (isHuman)
         cout << "Введите N: ";
@@ -70,7 +89,11 @@ int main(int argc, const char* argv[]) {
     if (isHuman)
         cout << "Введите M: ";
     cin >> M;
-    double A1[MAX_SIZE][MAX_SIZE];
+    // Выделяение памяти для матрицы A1
+    double** A1 = new double* [N];
+    for (int i = 0; i < N; ++i) {
+        A1[i] = new double[M];
+    }
     if (isHuman)
         cout << "Введите элементы матрицы A1 построчно:\n";
     inputMatrix(A1, N, M);
@@ -91,27 +114,43 @@ int main(int argc, const char* argv[]) {
             if (isHuman)
                 cout << "Введите K: ";
             cin >> K;
-
-            double A2[MAX_SIZE][MAX_SIZE];
+            // Выделение памяти для матрицы А2
+            double** A2 = new double* [M];
+            for (int i = 0; i < M; ++i) {
+                A2[i] = new double[K];
+            }
             if (isHuman)
                 cout << "Введите элементы матрицы A2 размером " << M << " x " << K << " построчно:\n";
             inputMatrix(A2, M, K);
-
-            if (M != K) {
+            if (M != M) { 
                 if (isHuman)
                     cout << "Невозможно произвести операцию" << endl;
                 else
                     cout << "NO" << endl;
             }
             else {
-                double result[MAX_SIZE][MAX_SIZE];
+                // Результирующая матрица
+                double** result = new double* [N];
+                for (int i = 0; i < N; ++i) {
+                    result[i] = new double[K];
+                }
                 multiplyMatrix(A1, A2, result, N, M, K);
                 for (int i = 0; i < N; ++i) {
                     for (int j = 0; j < K; ++j) {
                         A1[i][j] = result[i][j];
                     }
                 }
+                // Освобождение памяти
+                for (int i = 0; i < N; ++i) {
+                    delete[] result[i];
+                }
+                delete[] result;
             }
+            // Освобождение памяти
+            for (int i = 0; i < M; ++i) {
+                delete[] A2[i];
+            }
+            delete[] A2;
         }
         else if (command == 3) {
             int x;
@@ -126,13 +165,22 @@ int main(int argc, const char* argv[]) {
                     cout << "NO" << endl;
             }
             else {
-                double result[MAX_SIZE][MAX_SIZE];
+                // Результирующая матрица
+                double** result = new double* [N];
+                for (int i = 0; i < N; ++i) {
+                    result[i] = new double[N];
+                }
                 matrixPower(A1, result, x, N);
                 for (int i = 0; i < N; ++i) {
                     for (int j = 0; j < N; ++j) {
                         A1[i][j] = result[i][j];
                     }
                 }
+                // Освобождение памяти
+                for (int i = 0; i < N; ++i) {
+                    delete[] result[i];
+                }
+                delete[] result;
             }
         }
         else {
@@ -140,5 +188,10 @@ int main(int argc, const char* argv[]) {
                 cout << "Некорректная команда." << endl;
         }
     }
+    // Освобождение памяти матрицы А1
+    for (int i = 0; i < N; ++i) {
+        delete[] A1[i];
+    }
+    delete[] A1;
     return 0;
 }
